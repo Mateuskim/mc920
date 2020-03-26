62acc78f19eb8189197b71cbf50df777d0ce14b3
MateusKim <mateuskimura@gmail.com>
2020-03-26 12:24:01 -0300

    image resolution algorithm done

--- trabalho1/imgRes.py
+++ trabalho1/imgRes.py
@@ -3,30 +3,34 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import sys
+import cv2
 
+def imageResolution(img):
+    scale_percent = 50  # percent of original size
+    j = 1
+    for i in range(6):
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-def imageResolution(img, res):
+        # resize image
+        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        new_img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_AREA)
 
-    size = 512,512
-    new_img = np.resize(img, size)
-    return new_img
+        new_name = "output/1.1/interaction " + str(j) + ".png"
+        # Saving image
+        plt.imsave(new_name, new_img, cmap='gray')
+        plt.imshow(new_img, cmap='gray')
+        plt.show()
+        j += 1
 
-
-def main(img_path, times):
+def main(img_path):
 
     # Reading image and applying filters
     img = misc.imread(img_path)
-    new_img = imageResolution(img, int(times))
-    new_name = "output/1.1/" + times + ".png"
-
-    # Saving image
-    plt.imsave(new_name, new_img, cmap='gray')
-
-    # showing the new image with the filter applied
-    plt.imshow(new_img, cmap='gray')
-    plt.show()
+    imageResolution(img)
 
 
 # Argument1 = image | Argument2 = times reduced
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
