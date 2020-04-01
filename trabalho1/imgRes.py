c261aef0e6f9bf0aaa86058cf85e81a41a896cd7
MateusKim <mateuskimura@gmail.com>
2020-04-01 17:12:18 -0300

    image resolutiom modified

--- trabalho1/imgRes.py
+++ trabalho1/imgRes.py
@@ -1,28 +1,25 @@
 from scipy import misc
 import matplotlib.pyplot as plt
-import math
 import numpy as np
 import sys
 import cv2
 
 def imageResolution(img):
-    scale_percent = 50  # percent of original size
-    j = 1
+    dimI = (img.shape[1], img.shape[0])
     for i in range(6):
-        width = int(img.shape[1] * scale_percent / 100)
-        height = int(img.shape[0] * scale_percent / 100)
+        width = int(img.shape[1]/2)
+        height = int(img.shape[0]/2)
         dim = (width, height)
 
         # resize image
         img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        new_img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_AREA)
+        new_img = cv2.resize(img, dimI, interpolation=cv2.INTER_AREA)
 
-        new_name = "output/1.1/interaction " + str(j) + ".png"
+        new_name = "output/1.1/" + str(int(512/2**(i+1))) +"x"+ str(int(512/2**(i+1))) + ".png"
         # Saving image
         plt.imsave(new_name, new_img, cmap='gray')
         plt.imshow(new_img, cmap='gray')
         plt.show()
-        j += 1
 
 def main(img_path):
 
