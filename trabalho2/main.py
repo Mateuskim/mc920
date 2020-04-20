20f928463d619c563e5ad956fdc6ccf6ddae5a73
Mateuskim <mateuskimura@gmail.com>
2020-04-20 14:08:30 -0300

    option for just 1 argv working and fix normalization

--- trabalho2/main.py
+++ trabalho2/main.py
@@ -4,13 +4,19 @@ import matplotlib.pyplot as plt
 
 from filters import *
 
+def main_all(img_path):
+    # reading the image
+    new_img = img = misc.imread(img_path)
+    for i in range(1,10):
+        new_img = 255*apply_filter(img/255, 'h'+str(i))
+        plt.imshow(new_img, cmap='gray', vmin=0, vmax=255)
+        plt.show()
 
 def main(img_path, function):
     # reading the image
     new_img = img = misc.imread(img_path)
 
-    new_img = apply_filter(new_img, function)
-
+    new_img = 255*apply_filter(new_img/255, function)
     # path and name of the new image
     new_name = "output/" + img_path[4:-4] + '-' + function + ".png"
 
@@ -22,4 +28,10 @@ def main(img_path, function):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 2:
+        main_all(sys.argv[1])
+    elif len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print("python3 main.py [IMG_PATH] [FILTER]*")
+        print("* - optional")
